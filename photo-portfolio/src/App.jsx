f4f7642bcfec2cc9d12c01363e307a238197bbf4
“jaironberiguete” <Jaironberiguete@gmail.com>
2025-07-10 00:20:28 -0400

    added masonry grid tool to fit better the pictures

--- photo-portfolio/src/App.jsx
+++ photo-portfolio/src/App.jsx
@@ -1,12 +1,17 @@
 import Navbar from './components/Navbar'
 import Gallery from './components/Gallery'
 import Footer from './components/Footer'
+import Hero from './components/Hero'
+import About from './components/About'
+import Contact from './components/Contact'
 
 function App() {
   return (
     <>
       <Navbar />
+      <Hero />
       <Gallery />
+      <Contact/>
       <Footer />
     </>
   )
--- photo-portfolio/src/components/Gallery.jsx
+++ photo-portfolio/src/components/Gallery.jsx
@@ -1,4 +1,7 @@
-function Gallery() {
+import Masonry from 'react-masonry-css';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
+import 'react-photo-view/dist/react-photo-view.css';
+import './Gallery.css';
   const images = [
     '/images/photo1.jpg',
     '/images/photo31.jpg',
@@ -12,20 +15,37 @@ function Gallery() {
     '/images/photo17.jpg',
     '/images/photo19.jpg',
     '/images/photo22.jpg',
-  ]
+  ];
 
+  
+const breakpointColumnsObj = {
+  default: 4,
+  1100: 2,
+  700: 1
+};
+
+function Gallery() {
   return (
-   <section id="gallery" className="gallery-section">
+    <section id="gallery" className="gallery-section">
       <h2>GALERIA</h2>
-      <div className="gallery-grid">
-        {images.map((src, idx) => (
-          <div key={idx} className="gallery-item">
-            <img src={src} alt={`Gallery ${idx + 1}`} />
-          </div>
-        ))}
-      </div>
+
+      <PhotoProvider>
+        <Masonry
+          breakpointCols={breakpointColumnsObj}
+          className="masonry-grid"
+          columnClassName="masonry-column"
+        >
+          {images.map((src, idx) => (
+            <PhotoView key={idx} src={src}>
+              <div className="gallery-item">
+                <img src={src} alt={`Gallery ${idx + 1}`} />
+              </div>
+            </PhotoView>
+          ))}
+        </Masonry>
+      </PhotoProvider>
     </section>
-  )
+  );
 }
 
-export default Gallery
+export default Gallery;
